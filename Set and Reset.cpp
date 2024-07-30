b171f0a336264db7736baeadc57d35772a74e7e2
SiddhantBhosale05 <siddhant.bhosale05@gmail.com>
2024-07-30 12:20:40 +0530

    Add files via upload

---
+++ Set and Reset.cpp
@@ -0,0 +1,27 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Bitwise Operators Set and Reset
+
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int a,i,set,reset;
+    cout << "Enter the number";
+    cin >> a;
+    cout << "Enter the places to shift";
+    cin >> i;
+    set = a|(1<<i);
+    reset = a&(~(1<<i));
+    cout << reset<< endl;
+    cout << set;
+    
+    return 0;
+} 
+
+
+/*Output:
+Enter the number1
+Enter the places to shift1
+1
+3*/
